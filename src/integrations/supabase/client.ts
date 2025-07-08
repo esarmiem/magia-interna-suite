6c293b873a5826b0a131eb48bfc6658681bfa426
esarmiem <eldersarmiento1@gmail.com>
2025-07-07 21:35:15 -0500

    FIX errors in config files

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://wqeqkdgjkzrmxmhrfrmf.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndxZXFrZGdqa3pybXhtaHJmcm1mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE5MjkxNzksImV4cCI6MjA2NzUwNTE3OX0.8qWEyxt9tqEDMv6JQvXDshAQTRMB-DEhyAI4BPFPFSI";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL as string;
+const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
