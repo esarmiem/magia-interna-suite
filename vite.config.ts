c02faefd8fcb36ee2411d9e9d28320be85f6523b
esarmiem <eldersarmiento1@gmail.com>
2025-07-10 01:51:39 -0500

    ADD pwa feature

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -13,6 +14,30 @@ export default defineConfig(({ mode }) => ({
     react(),
     mode === 'development' &&
     componentTagger(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Magia Interna',
+        short_name: 'MagiaInterna',
+        description: 'Suite de gestión Magia Interna',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/spell-book192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/spell-book.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ].filter(Boolean),
   resolve: {
     alias: {
